b8f5235b7070dea77d7db8ce575931c20c730e7c
Johnny <ehuamanip@pucp.pe>
2015-01-28 18:19:05 -0500

    agregar variables de configuracion de BD

--- src/Core/DefaultBundle/Controller/DefaultController.php
+++ src/Core/DefaultBundle/Controller/DefaultController.php
@@ -8,6 +8,19 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CoreDefaultBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'Theme', 'isActive'=>1));
+        //obtenemos ĺa configuración
+        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'siteNameLarge', 'isActive'=>1));
+        return $this->render('CoreDefaultBundle:Default:index.html.twig', array(
+            'theme'=>$objTheme->getChrValue(),
+            'siteName'=>$objSite->getChrValue()
+        ));
+    }
+    
+    public function vd($var){
+        echo "<pre>";
+        print_r($var);
+        echo "</pre>";
     }
 }
