cdb50e43c4e45cda9c134b45463eb654802d754b
Ernesto Tarrillo Pérez <etarrillo@pucp.pe>
2015-02-02 17:51:49 -0500

    avanzando la parte del frontpage

--- src/Core/DefaultBundle/Controller/DefaultController.php
+++ src/Core/DefaultBundle/Controller/DefaultController.php
@@ -17,7 +17,30 @@ class DefaultController extends Controller
             'siteName'=>$objSite->getChrValue()
         ));
     }
-    
+
+    public function descargasAction(){
+        $em = $this->getDoctrine()->getManager();
+        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'Theme', 'isActive'=>1));
+        //obtenemos ĺa configuración
+        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'siteNameLarge', 'isActive'=>1));
+        return $this->render('CoreDefaultBundle:Default:descargas.html.twig', array(
+            'theme'=>$objTheme->getChrValue(),
+            'siteName'=>$objSite->getChrValue()
+        ));
+    }
+
+    public function documentacionAction(){
+        $em = $this->getDoctrine()->getManager();
+        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'Theme', 'isActive'=>1));
+        //obtenemos ĺa configuración
+        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'siteNameLarge', 'isActive'=>1));
+        return $this->render('CoreDefaultBundle:Default:documentacion.html.twig', array(
+            'theme'=>$objTheme->getChrValue(),
+            'siteName'=>$objSite->getChrValue()
+        ));
+    }
+
+
     public function vd($var){
         echo "<pre>";
         print_r($var);
