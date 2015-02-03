5cb2224a0dff3a810538058a7d8114dca67d7cd7
Johnny <ehuamanip@pucp.pe>
2015-02-03 16:18:44 -0500

    vista de documentacion de los desarrollos

--- src/Core/DefaultBundle/Controller/DefaultController.php
+++ src/Core/DefaultBundle/Controller/DefaultController.php
@@ -11,9 +11,11 @@ class DefaultController extends Controller {
         $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
         //obtenemos ĺa configuración
         $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
+        $activeMenu = $this->getClassMenuActive('home');
         return $this->render('CoreDefaultBundle:Default:index.html.twig', array(
                     'theme' => $objTheme->getChrValue(),
-                    'siteName' => $objSite->getChrValue()
+                    'siteName' => $objSite->getChrValue(),
+                    'menuActive' => $activeMenu
         ));
     }
 
@@ -22,46 +24,24 @@ class DefaultController extends Controller {
         $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
         //obtenemos ĺa configuración
         $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
+        $activeMenu = $this->getClassMenuActive('descargas');
         return $this->render('CoreDefaultBundle:Default:descargas.html.twig', array(
                     'theme' => $objTheme->getChrValue(),
-                    'siteName' => $objSite->getChrValue()
+                    'siteName' => $objSite->getChrValue(),
+                    'menuActive' => $activeMenu
         ));
     }
 
-    public function documentacionAction($project) {
+    public function manualesAction() {
         $em = $this->getDoctrine()->getManager();
         $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
         //obtenemos ĺa configuración
         $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
-        return $this->render('CoreDefaultBundle:Default:documentacion.html.twig', array(
+        $activeMenu = $this->getClassMenuActive('manuales');
+        return $this->render('CoreDefaultBundle:Default:manuales.html.twig', array(
                     'theme' => $objTheme->getChrValue(),
                     'siteName' => $objSite->getChrValue(),
-                    'project'=>$project
-        ));
-    }
-    public function viewdocAction($project, $type, $name) {
-        $em = $this->getDoctrine()->getManager();
-        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
-        //obtenemos ĺa configuración
-        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
-        $urlDoc = '/documentation/'.$project.'/'.$type.'/'.$name.'/index.html';
-        return $this->render('CoreDefaultBundle:Default:viewdoc.html.twig', array(
-                    'theme' => $objTheme->getChrValue(),
-                    'siteName' => $objSite->getChrValue(),
-                    'project'=>$project,
-                    'type'=>$type,
-                    'urlDoc'=>$urlDoc
-        ));
-    }
-
-    public function catalogoAction() {
-        $em = $this->getDoctrine()->getManager();
-        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
-        //obtenemos ĺa configuración
-        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
-        return $this->render('CoreDefaultBundle:Default:catalogo.html.twig', array(
-                    'theme' => $objTheme->getChrValue(),
-                    'siteName' => $objSite->getChrValue()
+                    'menuActive' => $activeMenu
         ));
     }
 
@@ -70,12 +50,55 @@ class DefaultController extends Controller {
         $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
         //obtenemos ĺa configuración
         $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
-        //$iframe = $this->get('kernel')->locateResource('@CoreDefaultBundle');
-        $iframe='/documentation/bcp/local/library/index.html';
+        $activeMenu = $this->getClassMenuActive('involucrarse');
         return $this->render('CoreDefaultBundle:Default:involucrarse.html.twig', array(
                     'theme' => $objTheme->getChrValue(),
                     'siteName' => $objSite->getChrValue(),
-                    'iframe'=>$iframe
+                    'menuActive' => $activeMenu
+        ));
+    }
+
+    public function catalogoAction() {
+        $em = $this->getDoctrine()->getManager();
+        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
+        //obtenemos ĺa configuración
+        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
+        $activeMenu = $this->getClassMenuActive('catalogo');
+        return $this->render('CoreDefaultBundle:Default:catalogo.html.twig', array(
+                    'theme' => $objTheme->getChrValue(),
+                    'siteName' => $objSite->getChrValue(),
+                    'menuActive' => $activeMenu
+        ));
+    }
+
+    public function documentacionAction($project) {
+        $em = $this->getDoctrine()->getManager();
+        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
+        //obtenemos ĺa configuración
+        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
+        $activeMenu = $this->getClassMenuActive('documentacion');
+        return $this->render('CoreDefaultBundle:Default:documentacion.html.twig', array(
+                    'theme' => $objTheme->getChrValue(),
+                    'siteName' => $objSite->getChrValue(),
+                    'project' => $project,
+                    'menuActive' => $activeMenu
+        ));
+    }
+
+    public function viewdocAction($project, $type, $name) {
+        $em = $this->getDoctrine()->getManager();
+        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
+        //obtenemos ĺa configuración
+        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
+        $urlDoc = '/documentation/' . $project . '/' . $type . '/' . $name . '/index.html';
+        $activeMenu = $this->getClassMenuActive('documentacion');
+        return $this->render('CoreDefaultBundle:Default:viewdoc.html.twig', array(
+                    'theme' => $objTheme->getChrValue(),
+                    'siteName' => $objSite->getChrValue(),
+                    'project' => $project,
+                    'type' => $type,
+                    'urlDoc' => $urlDoc,
+                    'menuActive' => $activeMenu
         ));
     }
 
@@ -88,6 +111,23 @@ class DefaultController extends Controller {
 //                    'theme' => $objTheme->getChrValue(),
 //                    'siteName' => $objSite->getChrValue()
 //        ));
+    }
+
+    private function getClassMenuActive($menu) {
+        $arrayMenu = array(
+            'descargas' => '',
+            'manuales' => '',
+            'involucrarse' => '',
+            'catalogo' => '',
+            'documentacion' => ''
+        );
+        foreach ($arrayMenu as $index => $strMenu) {
+            if ($menu == $index) {
+                $arrayMenu[$index] = 'active';
+                break;
+            }
+        }
+        return $arrayMenu;
     }
 
     public function vd($var) {
