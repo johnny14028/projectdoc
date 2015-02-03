d9bcc417ac6661735ba69114c7f5e0b5a6b4a6b2
Johnny <ehuamanip@pucp.pe>
2015-02-03 10:41:33 -0500

    nuevas paginas para el menu

--- src/Core/DefaultBundle/Controller/DefaultController.php
+++ src/Core/DefaultBundle/Controller/DefaultController.php
@@ -4,46 +4,77 @@ namespace Core\DefaultBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
+class DefaultController extends Controller {
+
+    public function indexAction() {
         $em = $this->getDoctrine()->getManager();
-        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'Theme', 'isActive'=>1));
+        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
         //obtenemos ĺa configuración
-        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'siteNameLarge', 'isActive'=>1));
+        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
         return $this->render('CoreDefaultBundle:Default:index.html.twig', array(
-            'theme'=>$objTheme->getChrValue(),
-            'siteName'=>$objSite->getChrValue()
+                    'theme' => $objTheme->getChrValue(),
+                    'siteName' => $objSite->getChrValue()
         ));
     }
 
-    public function descargasAction(){
+    public function descargasAction() {
         $em = $this->getDoctrine()->getManager();
-        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'Theme', 'isActive'=>1));
+        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
         //obtenemos ĺa configuración
-        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'siteNameLarge', 'isActive'=>1));
+        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
         return $this->render('CoreDefaultBundle:Default:descargas.html.twig', array(
-            'theme'=>$objTheme->getChrValue(),
-            'siteName'=>$objSite->getChrValue()
+                    'theme' => $objTheme->getChrValue(),
+                    'siteName' => $objSite->getChrValue()
         ));
     }
 
-    public function documentacionAction(){
+    public function documentacionAction() {
         $em = $this->getDoctrine()->getManager();
-        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'Theme', 'isActive'=>1));
+        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
         //obtenemos ĺa configuración
-        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName'=>'siteNameLarge', 'isActive'=>1));
+        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
         return $this->render('CoreDefaultBundle:Default:documentacion.html.twig', array(
-            'theme'=>$objTheme->getChrValue(),
-            'siteName'=>$objSite->getChrValue()
+                    'theme' => $objTheme->getChrValue(),
+                    'siteName' => $objSite->getChrValue()
         ));
     }
 
+    public function catalogoAction() {
+        $em = $this->getDoctrine()->getManager();
+        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
+        //obtenemos ĺa configuración
+        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
+        return $this->render('CoreDefaultBundle:Default:catalogo.html.twig', array(
+                    'theme' => $objTheme->getChrValue(),
+                    'siteName' => $objSite->getChrValue()
+        ));
+    }
 
-    public function vd($var){
+    public function involucrarseAction() {
+        $em = $this->getDoctrine()->getManager();
+        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
+        //obtenemos ĺa configuración
+        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
+        return $this->render('CoreDefaultBundle:Default:involucrarse.html.twig', array(
+                    'theme' => $objTheme->getChrValue(),
+                    'siteName' => $objSite->getChrValue()
+        ));
+    }
+    public function buscarAction() {
+        $em = $this->getDoctrine()->getManager();
+//        $objTheme = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'Theme', 'isActive' => 1));
+        //obtenemos ĺa configuración
+//        $objSite = $em->getRepository('CoreManagerBundle:Config')->findOneBy(array('chrName' => 'siteNameLarge', 'isActive' => 1));
+//        return $this->render('CoreDefaultBundle:Default:involucrarse.html.twig', array(
+//                    'theme' => $objTheme->getChrValue(),
+//                    'siteName' => $objSite->getChrValue()
+//        ));
+    }
+
+    public function vd($var) {
         echo "<pre>";
         print_r($var);
         echo "</pre>";
     }
+
 }
